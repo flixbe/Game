357fd744857c4ff9fc6c57a85be2e64b412cb688
krishtal <krishtalforkato@gmail.com>
2019-03-04 20:20:59 +0200

    Mob

---
+++ src/core/entity/mob/Mob.java
@@ -0,0 +1,37 @@
+package core.entity.mob;
+
+import core.entity.Entity;
+import core.graphics.Sprite;
+
+public abstract class Mob extends Entity {
+
+	protected Sprite sprite;
+	protected int direction = 0;
+	protected boolean moving = false;
+	
+	public void move(int xAxis, int yAxis) {
+		if (xAxis > 0) direction = 1;
+		if (xAxis < 0) direction = 2;
+		if (yAxis > 0) direction = 3;
+		if (yAxis < 0) direction = 0;
+		
+		
+		if (!collision()) {
+			x += xAxis;
+			y += yAxis;	
+		}
+	}
+	
+	public void update() {
+		
+	}
+	
+	public void render() {
+		
+	}
+	
+	private boolean collision() {
+		return false;
+	}
+	
+}
